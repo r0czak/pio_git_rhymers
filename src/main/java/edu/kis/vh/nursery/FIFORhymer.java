e0a030101fac19050fb05735ee5787773dd175b8
Bartosz Kaczor <222453@edu.p.lodz.pl>
2020-12-16 11:58:40 +0100

    1.3 - Rename classes
    Rename classes using Java convention

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,9 +1,9 @@
 package edu.kis.vh.nursery;
 
-public class FIFORhymer extends defaultCountingOutRhymer {
+public class FIFORhymer extends DefaultCountingOutRhymer {
 
-  public defaultCountingOutRhymer baseRhymer = new defaultCountingOutRhymer();
-
+  public DefaultCountingOutRhymer baseRhymer = new DefaultCountingOutRhymer();
+  
   @Override
   public int countOut() {
     while (!callCheck())
