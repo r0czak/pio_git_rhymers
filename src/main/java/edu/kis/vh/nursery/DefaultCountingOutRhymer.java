faa9faaf5a3a376821740afd5a36195238e65d38
matim1324 <matim1324@gmail.com>
2020-12-16 15:02:18 +0100

    1.4.2 Change constants in DefaultCountingOutRhymer and HanoiRhymer, add final modifier and use an @Override annotation.

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,11 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-  private int[] numbers = new int[12];
+  public static final int size = 12;
+  public static final int error = -1;
+  private final int[] numbers = new int[size];
 
-  public int total = -1;
+  public int total = error;
 
   public void countIn(int in) {
     if (!isFull())
@@ -12,22 +14,22 @@ public class DefaultCountingOutRhymer {
   }
 
   public boolean callCheck() {
-    return total == -1;
+    return total == error;
   }
 
   public boolean isFull() {
-    return total == 11;
+    return total == size - 1;
   }
 
   protected int peekaboo() {
     if (callCheck())
-      return -1;
+      return error;
     return numbers[total];
   }
 
   public int countOut() {
     if (callCheck())
-      return -1;
+      return error;
     return numbers[total--];
   }
 
